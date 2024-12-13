716f5e04ee599c620ce27f79fdd570b6bfda7ab7
Awiral Chand <103342572+awi246@users.noreply.github.com>
2024-12-13 13:54:54 +0545

    loader z index

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 
 const Loader = () => {
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-white bg-opacity-50">
+    <div className="fixed inset-0 flex justify-center items-center bg-white bg-opacity-50 z-50">
       <div className="flex flex-row gap-2">
         <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce z-50"></div>
         <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce z-50" style={{ animationDelay: '-0.3s' }}></div>
